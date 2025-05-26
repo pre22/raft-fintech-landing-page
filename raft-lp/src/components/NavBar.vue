1482b7b533a02a232f8e73deb66a7b4a51b8a0a2
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-05-26 21:48:51 +0100

    refactored nav bar component

--- raft-lp/src/components/NavBar.vue
+++ raft-lp/src/components/NavBar.vue
@@ -1,20 +1,31 @@
 <template>
-  <nav>
+  <nav class="flex justify-between content-center text-white nav-bd pb-6">
     <!-- logo  -->
     <div>
-      <img src="" alt="Raft" srcset="" />
+      <img src="/src/assets/img/raft.png" alt="Raft" srcset="" />
     </div>
-    <RouterLink to="/">Solutions</RouterLink>
-    <RouterLink to="/">Learn</RouterLink>
-    <RouterLink to="/">About</RouterLink>
+
+    <div class="flex justify-between custom-text-gray">
+      <RouterLink to="/" class="px-4">Solutions</RouterLink>
+      <RouterLink to="/" class="px-4">Learn</RouterLink>
+      <RouterLink to="/" class="px-4">About</RouterLink>
+    </div>
 
     <!-- buttons  -->
     <div>
-      <button>Login</button>
-      <button>Get Started</button>
+      <button type="button" class="py-2 px-3 font-semibold">Login</button>
+      <button type="button" class="custom-btn-bg-green py-2 px-3 font-semibold border-radius-100">
+        Get Started
+      </button>
     </div>
   </nav>
 </template>
+
+<style scoped>
+.nav-bd {
+  border-bottom: 1px solid #3d3d3d;
+}
+</style>
 
 <script setup>
 import { RouterLink } from 'vue-router'
