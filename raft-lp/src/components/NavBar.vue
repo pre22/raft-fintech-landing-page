3d4f8eebcf3d5093849ac282e197edfe2163abd7
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-05-26 20:26:02 +0100

    working on nav section

---
+++ raft-lp/src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <!-- logo  -->
+    <div>
+      <img src="" alt="Raft" srcset="" />
+    </div>
+    <RouterLink to="/">Solutions</RouterLink>
+    <RouterLink to="/">Learn</RouterLink>
+    <RouterLink to="/">About</RouterLink>
+
+    <!-- buttons  -->
+    <div>
+      <button>Login</button>
+      <button>Get Started</button>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
