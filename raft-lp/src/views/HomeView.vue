67a356db80665767e7b4163e229cbbaa69f02471
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-05-26 21:55:31 +0100

    working on hero section

--- raft-lp/src/views/HomeView.vue
+++ raft-lp/src/views/HomeView.vue
@@ -1,9 +1,30 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="">
+    <NavBar />
+
+    <!-- Hero section  -->
+    <div class="grid gap-y-[32px] py-13 text-center font-normal">
+      <button type="button">Introducing Rafts Card</button>
+
+      <h1 class="text-white text-8xl">Building the future of banking</h1>
+
+      <p class="custom-text-gray text-2xl">
+        Experience the future of banking with RAFT. We're here <br />
+        to empower your financial journey.
+      </p>
+
+      <div>
+        <button
+          type="button"
+          class="custom-btn-bg-green text-white py-4 px-8 font-semibold border-radius-100"
+        >
+          Get Started
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+</script>
