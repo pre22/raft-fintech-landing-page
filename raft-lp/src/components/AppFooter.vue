f22ec201d6a7d462213033350b7ce6662010054a
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-05-27 19:14:43 +0100

    renamed component

---
+++ raft-lp/src/components/AppFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer>
+    <div>
+      <img
+        src="/src/assets/img/raft.png"
+        alt="Raft"
+        srcset=""
+        style="max-height: 153px; max-width: 363.82px"
+      />
+    </div>
+  </footer>
+</template>
