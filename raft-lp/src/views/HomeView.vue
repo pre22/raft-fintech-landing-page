1571cb806f9f3c2d58de2b80615f8e5473cc5797
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-05-27 19:05:42 +0100

    Update HomeView.vue

--- raft-lp/src/views/HomeView.vue
+++ raft-lp/src/views/HomeView.vue
@@ -3,8 +3,16 @@
     <NavBar />
 
     <!-- Hero section  -->
-    <div class="grid gap-y-[32px] py-13 text-center font-normal">
-      <button type="button">Introducing Rafts Card</button>
+    <div class="grid gap-y-[32px] py-18 pb-30 text-center font-normal">
+      <div>
+        <button
+          type="button"
+          class="text-[#DCDCDC] bg-[#989898] px-4 py-2 border-radius-100 text-base"
+        >
+          Introducing Rafts Card
+          <i class="fa-solid fa-chevron-right pl-[10px]"></i>
+        </button>
+      </div>
 
       <h1 class="text-white text-8xl">Building the future of banking</h1>
 
@@ -21,6 +29,66 @@
           Get Started
         </button>
       </div>
+    </div>
+
+    <!-- img  -->
+    <div class="width-90">
+      <img
+        src="/src/assets/img/frame_one.png"
+        alt="happy"
+        style="max-height: 700px; max-width: 1360px; border-radius: 12px"
+      />
+    </div>
+
+    <!-- featured section  -->
+    <div class="text-center py-30">
+      <p class="font-medium custom-text-gray text-xl">Featured and seen in</p>
+      <div class="flex justify-center">
+        <div class="flex gap-x-[80px] items-center" style="max-width: 860px; max-height: 60px">
+          <img src="/src/assets/img/forbes.png" alt="forbes" />
+          <img src="/src/assets/img/cnbc.png" alt="cnbc" />
+          <img src="/src/assets/img/bloomberg.png" alt="bloomberg" />
+          <img src="/src/assets/img/reuters.png" alt="reuters" />
+          <img src="/src/assets/img/cnn.png" alt="cnn" />
+        </div>
+      </div>
+    </div>
+
+    <!-- elevate section  -->
+    <div class="grid gap-y-[32px] text-center py-30 width-90">
+      <h2 class="text-8xl text-white">Elevate Your Financial Journey with RAFT</h2>
+      <p class="text-xl leading-28px text-[#989898]">
+        RAFT offers a world of financial possibilities. From investments to payments,<br />
+        we've got you covered. Join us and unlock your financial potential today.
+      </p>
+    </div>
+
+    <!-- freedom section  -->
+    <div class="grid gap-y-[32px] text-center py-30 width-90">
+      <h2 class="text-8xl text-white">Your Financial Freedom, Your Way</h2>
+      <p class="text-xl leading-28px text-[#989898]">
+        We believe that managing your finances should be effortless and cost-effective. <br />
+        That's why we offer you the freedom you deserve
+      </p>
+    </div>
+
+    <!-- smart investment  -->
+    <div class="bg-[#48D64C] py-[132px]">
+      <h2 class="text-9xl width-90 text-[#070606]">
+        Smart investments, secure payments, and expert guidance, all in one place.
+      </h2>
+    </div>
+
+    <!-- confi section  -->
+    <div class="grid gap-y-[24px] text-left pt-[124px] width-90">
+      <h2 class="text-7xl text-white">
+        Confidently Shape Your <br />
+        Financial Future
+      </h2>
+      <p class="text-xl leading-28px text-[#989898]">
+        At RAFT, we empower you to confidently shape your financial future.Our modern <br />
+        approach simplifies saving and investing, making it easier than ever.
+      </p>
     </div>
   </div>
 </template>
