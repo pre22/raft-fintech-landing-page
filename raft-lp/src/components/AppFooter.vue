db2b19c387d0ef90ebd8daf9305f759ff2a87168
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-05-27 20:05:09 +0100

    working on page footer

--- raft-lp/src/components/AppFooter.vue
+++ raft-lp/src/components/AppFooter.vue
@@ -1,12 +1,60 @@
 <template>
-  <footer>
-    <div>
+  <footer class="text-white grid gap-[60px]">
+    <div class="flex">
       <img
-        src="/src/assets/img/raft.png"
+        class="max-w-sm min-w-[212px] max-h-[153px] min-h-[86px]"
+        src="/src/assets/img/raft.svg"
         alt="Raft"
         srcset=""
-        style="max-height: 153px; max-width: 363.82px"
       />
+    </div>
+
+    <div class="flex justify-between pt-[52px] border-t-1 border-[#3D3D3D]">
+      <!-- qr  -->
+      <div class="flex gap-3 px-4 py-5 rounded-xl border-2 border-dashed lg:min-w-[457px]">
+        <div>
+          <img src="/src/assets/img/qr_code_download.svg" alt="download_qr_code" />
+        </div>
+
+        <div class="flex flex-col gap-4">
+          <div>
+            <p>
+              Scan to download App on the <br />
+              Playstore and Appstore.
+            </p>
+          </div>
+          <div class="flex">
+            <img src="/src/assets/img/playstore_logo.svg" alt="android" />
+            <img src="/src/assets/img/apple_logo.svg" alt="ios" />
+          </div>
+        </div>
+      </div>
+
+      <!-- categories  -->
+      <div class="grid grid-cols-3 gap-[52px]">
+        <!-- about us  -->
+        <div class="grid gap-4">
+          <h6>About US</h6>
+          <p>Our Company</p>
+          <p>Careers</p>
+          <p>Press Kits</p>
+        </div>
+
+        <!-- Legal  -->
+        <div class="grid gap-4">
+          <h6>Legal</h6>
+          <p>Terms of use</p>
+          <p>Privacy Policy</p>
+          <p>About US</p>
+        </div>
+
+        <!-- Support  -->
+        <div class="grid gap-4">
+          <h6>Support</h6>
+          <p>Contact us</p>
+          <p>FAQ</p>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
