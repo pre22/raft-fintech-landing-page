79e0bc7eb40add76293b756f1465a1c3472c6f02
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-05-28 01:03:46 +0100

    Update HomeView.vue

--- raft-lp/src/views/HomeView.vue
+++ raft-lp/src/views/HomeView.vue
@@ -14,7 +14,10 @@
         </button>
       </div>
 
-      <h1 class="text-white text-8xl">Building the future of banking</h1>
+      <h1 class="text-white text-8xl">
+        Building the future of <br />
+        banking
+      </h1>
 
       <p class="custom-text-gray text-2xl">
         Experience the future of banking with RAFT. We're here <br />
@@ -32,7 +35,7 @@
     </div>
 
     <!-- img  -->
-    <div class="width-90">
+    <div class="width-90 flex justify-center">
       <img
         src="/src/assets/img/frame_one.png"
         alt="happy"
@@ -57,7 +60,10 @@
     <!-- elevate section  -->
     <div class="grid py-30 width-90">
       <div class="grid gap-y-[32px] pb-[108px] text-center">
-        <h2 class="text-8xl text-white">Elevate Your Financial Journey with RAFT</h2>
+        <h2 class="text-8xl text-white">
+          Elevate Your Financial <br />
+          Journey with RAFT
+        </h2>
         <p class="text-xl leading-28px text-[#989898]">
           RAFT offers a world of financial possibilities. From investments to payments,<br />
           we've got you covered. Join us and unlock your financial potential today.
@@ -88,8 +94,8 @@
             <div class="grid gap-y-4 p-[40px]">
               <h3 class="text-white font-semibold text-4xl">Smart Investing</h3>
               <p class="custom-text-gray">
-                Grow your wealth confidently with our personalized investment solutions, tailored to
-                your financial goals.
+                Grow your wealth confidently with our personalized <br />
+                investment solutions, tailored to your financial goals.
               </p>
             </div>
           </div>
@@ -98,21 +104,21 @@
         <div class="flex justify-center text-left gap-x-5">
           <div class="grid gap-y-[84px] pt-[48px] grid-background bg-[#131313] rounded-xl">
             <div class="flex justify-center pl-[33px] pt-[54px]">
-              <img src="/src/assets/img/smart_investment.svg" alt="" />
+              <img src="/src/assets/img/frame_106.svg" alt="" />
             </div>
 
             <div class="grid gap-y-4 p-[40px]">
               <h3 class="text-white font-semibold text-4xl">Wealth Management</h3>
               <p class="custom-text-gray">
-                Make informed decisions for your financial future with our wealth management
-                expertise.
+                Make informed decisions for your financial future with <br />
+                our wealth management expertise.
               </p>
             </div>
           </div>
 
           <div class="grid gap-y-[84px] pt-[48px] lg:min-w-[822px] grid-background rounded-xl">
             <div class="flex justify-center">
-              <img src="/src/assets/img/seamless_payment.svg" alt="" />
+              <img src="/src/assets/img/Group_8.svg" alt="" />
             </div>
 
             <div class="grid gap-y-4 p-[40px]">
@@ -128,19 +134,89 @@
     </div>
 
     <!-- freedom section  -->
-    <div class="grid gap-y-[32px] text-center py-30 width-90">
-      <h2 class="text-8xl text-white">Your Financial Freedom, Your Way</h2>
-      <p class="text-xl leading-28px text-[#989898]">
-        We believe that managing your finances should be effortless and cost-effective. <br />
-        That's why we offer you the freedom you deserve
-      </p>
+    <div class="grid text-center py-30 width-90">
+      <div class="grid gap-y-[32px] pb-[124px]">
+        <h2 class="text-[76px] text-white">
+          Your Financial Freedom,<br />
+          Your Way
+        </h2>
+        <p class="text-xl leading-28px text-[#989898]">
+          We believe that managing your finances should be effortless and cost-effective. <br />
+          That's why we offer you the freedom you deserve
+        </p>
+      </div>
+
+      <div class="flex flex-col gap-y-[80px]">
+        <div class="flex justify-center">
+          <img
+            src="/src/assets/img/Frame_31.png"
+            alt="happy"
+            style="max-height: 615px; max-width: 1296px; border-radius: 12px"
+          />
+        </div>
+
+        <!-- content  -->
+        <div class="text-white text-left flex justify-center">
+          <div class="grid gap-y-3">
+            <!-- first row -->
+            <div class="flex items-center gap-x-2">
+              <!-- icon  -->
+              <i class="fa-solid fa-money-bills"></i>
+              <p>No minimum balance fees</p>
+            </div>
+            <!-- second row  -->
+            <div>
+              <p class="text-base custom-text-gray leading-[24px]">
+                Say goodbye to minimum balance fees. Your account, your <br />
+                balance—no hidden charges
+              </p>
+            </div>
+          </div>
+
+          <div class="grid gap-y-3">
+            <!-- first row -->
+            <div class="flex items-center gap-x-2">
+              <!-- icon  -->
+              <i class="fa-solid fa-coins"></i>
+              <p>No monthly fees</p>
+            </div>
+            <!-- second row  -->
+            <div>
+              <p class="text-base custom-text-gray leading-[24px]">
+                Bank with us without worrying about monthly fees. Keep more <br />
+                of your money where it belongs—in your account
+              </p>
+            </div>
+          </div>
+
+          <div class="grid gap-y-3">
+            <!-- first row -->
+            <div class="flex items-center gap-x-2">
+              <!-- icon  -->
+              <i class="fa-solid fa-right-left"></i>
+              <p>No bank transfer fees</p>
+            </div>
+            <!-- second row  -->
+            <div>
+              <p class="text-base custom-text-gray leading-[24px]">
+                Seamlessly transfer funds without the extra cost. Send and <br />
+                receive money with zero bank transfer fees.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <!-- smart investment  -->
     <div class="bg-[#48D64C] py-[132px]">
-      <h2 class="text-9xl width-90 text-[#070606]">
-        Smart investments, secure payments, and expert guidance, all in one place.
-      </h2>
+      <div class="width-90">
+        <h2 class="text-9xl text-[#070606] xl:text-left">
+          Smart investments,<br />
+          secure payments,<br />
+          and expert guidance, all in one place.
+        </h2>
+      </div>
     </div>
 
     <!-- confi section  -->
